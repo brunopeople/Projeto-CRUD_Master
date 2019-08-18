6cc2639d05fcfbe3a43fc309e1495390902f6b32
brunopeople <brunopessoa.melo@gmail.com>
2019-08-17 22:58:36 -0300

    API Funcionando

--- restfull/index.js
+++ restfull/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+
 const consign = require('consign');
+
 const bodyParser = require('body-parser');
+
 const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
+
 app.use(expressValidator());
 
 
--- restfull/utils/validator.js
+++ restfull/utils/validator.js
@@ -1,7 +1,14 @@
 module.exports ={
 	pokemon:(app, req, res)=>{
+
 		req.assert('name','O nome é obrigatorio.').notEmpty();
-		req.assert('tipo','O tipo está invalido.').notEmpty().isEmail();
+		req.assert('imagem','imagem não pode ser nula').notEmpty();
+		req.assert('generation','Geração não pode ser nula').notEmpty();
+		req.assert('stage', 'Especifique o estágio do pokemon').notEmpty();
+		req.assert('type','Especifique o tipo do pokemon').notEmpty();
+
+		
+		
 	
 		let errors = req.validationErrors();
 
